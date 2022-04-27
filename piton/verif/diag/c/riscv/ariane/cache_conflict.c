70b56f1e3438cef73b5aa31f02ef3ff6ba19a15e
Yu-An-Shih <ianshih1998@gmail.com>
2022-04-26 22:08:17 -0400

    store evicted data in vc

--- piton/verif/diag/c/riscv/ariane/cache_conflict.c
+++ piton/verif/diag/c/riscv/ariane/cache_conflict.c
@@ -16,21 +16,21 @@
 
 int main(int argc, char ** argv) {
 
-  volatile int32_t* a1 = (int32_t*)0x000A0000;
-  volatile int32_t* a2 = (int32_t*)0x000A1000;
-  volatile int32_t* a3 = (int32_t*)0x000A2000;
-  volatile int32_t* a4 = (int32_t*)0x000A3000;
-  volatile int32_t* a5 = (int32_t*)0x000A4000;
+  int32_t* a1 = (int32_t*)0x84000000;
+  int32_t* a2 = (int32_t*)0x84100000;
+  int32_t* a3 = (int32_t*)0x84200000;
+  int32_t* a4 = (int32_t*)0x84300000;
+  //int32_t* a5 = (int32_t*)0x84400000;
 
-  int32_t tmp;
+  int32_t tmp = 0;
+  
+  perf_marker( 1555 );
 
   *a1 = 1;
   *a2 = 2;
   *a3 = 3;
   *a4 = 4;
-  *a5 = 5;
-  
-  perf_marker( 1555 );
+  //*a5 = 5;
 
   for (int32_t k = 0; k < 10; k++) {
     /**a1 = k;
@@ -39,18 +39,19 @@ int main(int argc, char ** argv) {
     *a4 = k;*/
     //*a5 = k;
     
-    tmp = *a1;
-    tmp = *a2;
-    tmp = *a3;
-    tmp = *a4;
-    tmp = *a5;
+    tmp += *a1;
+    tmp += *a2;
+    tmp += *a3;
+    tmp += *a4;
+    //tmp += *a5;
     
     //printf(*a1);
   }
-
   perf_marker( 1666 );
 
-  printf("Done!\n");
+  printf("%d\n", tmp);
 
+  printf("Done!\n");
+  
   return 0;
 }
